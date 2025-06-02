e6519999020310b7c873612cf59e5baac2791a20
Rimas Povilaitis <65122943+Rimantovas@users.noreply.github.com>
2025-06-02 19:10:08 +0300

    Update proto_gen.sh to specify protoc_plugin version 22.1.0 to avoid errors with later versions

--- packages/dart_wallet_core_codegen/tools/scripts/proto_gen.sh
+++ packages/dart_wallet_core_codegen/tools/scripts/proto_gen.sh
@@ -5,7 +5,8 @@ readonly OUT_DIR="./lib/src/proto"
 readonly PROTO_FILES="${PROTO_DIR}/*.proto"
 
 # if not install protobuf, install it by "brew install protobuf"
-flutter pub global activate protoc_plugin
+# using 22.1.0 because later versions produce errors
+flutter pub global activate protoc_plugin 22.1.0
 rm -rf ${OUT_DIR}
 mkdir -p ${OUT_DIR}
 protoc  -I=${PROTO_DIR} --dart_out=${OUT_DIR} ${PROTO_FILES}
